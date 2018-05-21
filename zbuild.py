a422787a9788cd8106d7c189386537ed8b3b7a87
Cameron Blackwood <cameron.blackwood@gmail.com>
2018-05-21 16:41:26 +1000

    working cards and split out code.

--- zbuild.py
+++ zbuild.py
@@ -4,13 +4,18 @@ import sys
 sys.path.append('..')
 
 import human2website.builder as builder
+import human2website.toolkits
 import human2website.toolkits.bootstrap4
+import human2website.toolkits.bootstrap4.dualmenu
 
 import xmenus
 
 
-toolkit=human2website.toolkits.bootstrap4.DualMenu()
+toolkit=human2website.toolkits.bootstrap4.dualmenu.DualMenu()
 
+
+
+ht=builder.HumanTree(toolkit,'webpages/','../2-24-battalion-org-au.github.io')
 
 
 toolkit.setting('menutop', toolkit.menu2html( xmenus.menutop ) )
@@ -18,10 +23,8 @@ toolkit.setting('menubottom', toolkit.menu2html( xmenus.menubottom ) )
 toolkit.setting('title','2-24th Battalion Association')
 toolkit.setting('keywords',"Tobruk, 2nd AIF, Tel El Eisa, El Alamein, Rats of Tobruk, World War 2, ANZAC, Australian Army, 9th Division, Salient, Halfaya Pass, Tarakan, New Guinea, Finschhafen, Lae, Wangaratta's Own")
 
-ht=builder.HumanTree(toolkit,'webpages/','../2-24-battalion-org-au.github.io')
 
 ht.precopy('/CNAME')
 ht.build_final()
-
 
 
