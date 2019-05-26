b80fbc443811cf0ff537a00bbda3a1d8e45ce6a6
Cameron Blackwood <cameron.blackwood@gmail.com>
2019-05-26 15:10:18 +1000

    add cmd for auto adding website build info

--- zbuild.py
+++ zbuild.py
@@ -25,6 +25,8 @@ toolkit.setting('title','2-24th Battalion Association')
 toolkit.setting('keywords',"Tobruk, 2nd AIF, Tel El Eisa, El Alamein, Rats of Tobruk, World War 2, ANZAC, Australian Army, 9th Division, Salient, Halfaya Pass, Tarakan, New Guinea, Finschhafen, Lae, Wangaratta's Own")
 
 
+ht.website_info('/whoami.txt',"i am generated via cam's magic and stored on appengine")
+
 ht.precopy('/CNAME')
 ht.build_final()
 
