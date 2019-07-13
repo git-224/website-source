f2abe03239014c1756c7fc6ea923f26cff1107eb
Cameron Blackwood <cameron.blackwood@gmail.com>
2019-07-13 23:40:50 +1000

    updated link checker url

--- tools/tool-linkcheck.sh
+++ tools/tool-linkcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-wget --spider -r -nd -nv -l 10  -o log-sitecheck-wget.log https://dev-github.battalion.org.au/index.html
+wget --spider -r -nd -nv -l 10  -o log-sitecheck-wget.log https://2-24.battalion.org.au/
 
 
